286451524396a3e53107c9f5083bee1a6c3becb5
kishore-kv <kmakireddi@kognivera.com>
2024-11-29 16:57:06 +0530

    added plus icon

--- src/_nav.js
+++ src/_nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 import {
+  cilPlus,
   cilBell,
   cilCalculator,
   cilChartPie,
@@ -11,7 +12,6 @@ import {
   cilNotes,
   cilPencil,
   cilPuzzle,
-  cilSpeedometer,
   cilStar,
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
@@ -21,7 +21,7 @@ const _nav = [
     component: CNavItem,
     name: 'Dashboard',
     to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+    icon: <CIcon icon={cilPlus} customClassName="nav-icon" />,
     badge: {
       color: 'info',
       text: 'NEW',
